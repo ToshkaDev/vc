d4d533e1ae9d252805f9a79693d21b81c1d5b4f5
Vadim Gumerov <netuns@gmail.com>
2025-05-16 15:24:39 -0400

    Add code to download genome assmebly report and the latest ncbi dbsnp database file and prepare them for common variant filtering

--- create_required_files.sh
+++ create_required_files.sh
@@ -85,3 +85,55 @@ if [ ! -f ${LCR_DIR}/${LCR_FILE%.*} ]; then
     gunzip -c ${LCR_DIR}/${LCR_FILE} > ${LCR_DIR}/${LCR_FILE%.*}
     rm ${LCR_DIR}/${LCR_FILE}
 fi
+
+################
+# Download the assembly report file to prepare a file containing the mapping of RefSeq genome assembly identifiers (e.g. GCF attachments) to chromosome names
+# To be used for filtering common snps from called variants
+ASSEMBLY_DIR=.${DATA_DIR}/assembly_mapping
+ASSEMBLY_VERSION=GCF_000001405.40_GRCh38.p14
+ASSEMBLY_REPORT_LINK=https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/${ASSEMBLY_VERSION}/${ASSEMBLY_VERSION}_assembly_report.txt
+ASSEMBLY_REPORT_FILE=assembly_report_file.tsv
+MAPPING_FILE=gcf_refseq2chr.tsv
+
+mkdir -p ${ASSEMBLY_DIR}
+
+if [ ! -f ${ASSEMBLY_DIR}/${MAPPING_FILE} ]; then
+    echo "Download the assembly report file to prepare a file containing the mapping of RefSeq genome assembly ids to chromosome names ..."
+    wget -O ${ASSEMBLY_DIR}/${ASSEMBLY_REPORT_FILE} ${ASSEMBLY_REPORT_LINK}
+    awk -F '\t' '$0 !~ /^#/ && $5 != "na" {print $7 "\t" $1}' ${ASSEMBLY_DIR}/${ASSEMBLY_REPORT_FILE} > ${ASSEMBLY_DIR}/${MAPPING_FILE}
+    rm ${ASSEMBLY_DIR}/${ASSEMBLY_REPORT_FILE}
+fi
+
+################
+# Downloading the latest NCBI dbsnp file for filtering common snps from called variants
+DBSNP_DIR=.${DATA_DIR}/ncbi_dbsnp
+DBSNP_LINK=https://ftp.ncbi.nih.gov/snp/latest_release/VCF/GCF_000001405.40.gz
+DBSNP_FILE=ncbi_dbsnp_latest.gz
+
+mkdir -p ${DBSNP_DIR}
+
+if [ ! -f ${DBSNP_DIR}/${DBSNP_FILE} ]; then
+    echo "Downloading gz compressed NBCI dbsnp database file (> 27.5 G, 10-20 min) ..."
+    wget -O ${DBSNP_DIR}/${DBSNP_FILE} ${DBSNP_LINK}
+
+    echo "Prepare file for filtering called variants (~10-15 min; requires RAM; can be helped through periodic cache dropping) ..."
+    zgrep "^#" ${DBSNP_DIR}/${DBSNP_FILE} > ${DBSNP_DIR}/${DBSNP_FILE%.*}.common.vcf
+
+    echo "Label COMMON for variants with AF>0.01 (1%) (10-15 min; requires RAM; can be helped through periodic cache dropping) ... "
+    zgrep ";COMMON" ${DBSNP_DIR}/${DBSNP_FILE} >> ${DBSNP_DIR}/${DBSNP_FILE%.*}.common.vcf
+
+    # get chr and position for common variants (>0.01)
+    refseq=$(cut -f1 ${ASSEMBLY_DIR}/${MAPPING_FILE})
+    IFS=' ' read -ra refseq <<< $(echo $refseq)
+    chr=$(cut -f2 ${ASSEMBLY_DIR}/${MAPPING_FILE})
+    IFS=$' ' read -ra chr <<< $(echo $chr)
+
+    touch ${DBSNP_DIR}/${DBSNP_FILE%.*}.common.chr.tsv
+
+    echo "Prpare a file with common variants replacing refseq ids with corresponding chromosome ids in the NCBI dbsnp file (~40 min) ..."
+    for i in ${!refseq[@]}; do
+        date
+        echo ${chr[$i]}
+        grep ${refseq[$i]} ${DBSNP_DIR}/${DBSNP_FILE%.*}.common.vcf | sed "s/${refseq[$i]}/${chr[$i]}/g" | cut -f1,2 >> ${DBSNP_DIR}/${DBSNP_FILE%.*}.common.chr.tsv
+    done
+fi
